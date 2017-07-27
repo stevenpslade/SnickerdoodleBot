96c4e1ff75c2121b26d5a6aa0eb0f6ea9eaf067b
stevenpslade <stevenpslade@gmail.com>
2017-07-26 19:17:22 -0700

    ensuring bot will comply with twitter guidelines, really only retweeting

--- lib/snickerdoodle_bot.rb
+++ lib/snickerdoodle_bot.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'twitter'
+require 'time'
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
@@ -8,4 +9,41 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
 
-client.update('Bring back snickerdoodle ice cream!')
+search_options = {
+  result_type: "recent"
+}
+
+found_snickerdoodle_tweet = false
+
+client.search("@rainorshineYVR+snickerdoodle -rt", search_options).each do |tweet|
+  if tweet.user.screen_name != "SnickerdooBot"
+    begin
+      client.retweet!(tweet)
+      client.favorite(tweet)
+      found_snickerdoodle_tweet = true
+      puts 'snickerdoodle tweet: ' + tweet.text
+    rescue
+     puts 'rescued from: ' + tweet.text
+    end
+  end
+end
+
+if !found_snickerdoodle_tweet
+  client.search("@rainorshineYVR -rt", search_options).each do |tweet|
+    if tweet.user.screen_name != "SnickerdooBot" && !tweet.reply?
+      one_day_ago = Time.now - (60 * 60 * 24)
+      tweet_time  = tweet.created_at - (60 * 60 * 7)
+      if tweet_time > one_day_ago
+
+        begin
+          client.update!("Rain or Shine Ice Cream needs to bring back snickerdoodle flavour!🍦👏 #BringBackSnickerdoodle " + tweet.url)
+          client.favorite(tweet)
+          puts 'tweet: ' + tweet.text
+        rescue
+          puts 'rescued from: ' + tweet.text
+        end
+
+      end
+    end
+  end
+end
